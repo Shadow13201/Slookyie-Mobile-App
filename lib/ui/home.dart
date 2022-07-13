106a1d831116fb2b45dbe6b4b45c547933bd330b
Shadow13201 <kunjapen@gmail.com>
2022-07-13 23:09:01 +0530

    max

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:slookyie_max/ui/staffselect.dart';
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -11,26 +12,72 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: Text("Home",style: TextStyle(color: Colors.black),),
       ),
-      backgroundColor: Colors.black,
-      body: Column(
-
+      backgroundColor: Colors.greenAccent,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+              physics: ClampingScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 8,
+              itemBuilder: (BuildContext context, int index){
+                return Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height/5,
+                  child: InkWell(
+                    onTap: ()=>Navigator.push(context, MaterialPageRoute(builder:(context)=>StaffSelect())),
+                    child: Card(
+                      color: Colors.purple,
+                      child: Stack(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage("assets/max2.png"),
+                                    fit: BoxFit.fitHeight
+                                )
+                            ),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(left: 10.0),
+                                child: Text("Cutting",style: TextStyle(color: Colors.black,fontSize: 21,fontWeight: FontWeight.bold)),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }
+            )
+          ],
+        ),
       ),
       bottomNavigationBar: BottomAppBar(
-        child: Row(
-          children: [
-            Icon(Icons.account_circle_sharp),
-            Spacer(),
-            Icon(Icons.account_circle_sharp)
-          ],
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height/13,
+          child: Row(
+            children: [
+              Icon(Icons.account_circle_sharp),
+              Spacer(),
+              Icon(Icons.account_circle_sharp)
+            ],
+          ),
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.white,
         onPressed: (){},
-        child: Icon(Icons.add),
+        child: Icon(Icons.add,color: Colors.purple,),
       ),
     );
   }
---
+++ lib/ui/staffselect.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class StaffSelect extends StatefulWidget {
+  const StaffSelect({Key? key}) : super(key: key);
+
+  @override
+  State<StaffSelect> createState() => _StaffSelectState();
+}
+
+class _StaffSelectState extends State<StaffSelect> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
