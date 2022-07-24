fd1cdc6dac1bb5c51fabcf0e8566989e5a878d8b
Shadow13201 <kunjapen@gmail.com>
2022-07-24 17:09:42 +0530

    max

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:slookyie_max/ui/staffselect.dart';
 
+import '../bloc/viewServicesBloc.dart';
 import 'profile.dart';
 
 class Home extends StatefulWidget {
@@ -27,54 +29,69 @@ class _HomeState extends State<Home> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ListView.builder(
-                physics: ClampingScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: 8,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height / 5,
-                    child: InkWell(
-                      onTap: () async {
-                        DateTime? newDate = await showDatePicker(
-                            context: context,
-                            initialDate: date,
-                            firstDate: DateTime(2001),
-                            lastDate: DateTime(2030)
-                        );
-                        if(newDate == null) return;
-                        setState(()=> date = newDate);
-                      },
-                      child: Card(
-                        color: Colors.purple,
-                        child: Stack(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image: AssetImage("assets/max2.png"),
-                                      fit: BoxFit.fitHeight)),
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
+            BlocBuilder<ViewServicesBloc, ViewServicesState>(
+                builder: (context, state) {
+              if (state is ViewServicesChecked) {
+                return ListView.builder(
+                    physics: ClampingScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: state.view!.data!.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height / 5,
+                        child: InkWell(
+                          onTap: () async {
+                            DateTime? newDate = await showDatePicker(
+                                context: context,
+                                initialDate: date,
+                                firstDate: DateTime(2001),
+                                lastDate: DateTime(2030));
+                            if (newDate == null) return;
+                            setState(() => date = newDate);
+                          },
+                          child: Card(
+                            color: Colors.purple,
+                            child: Stack(
                               children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 10.0),
-                                  child: Text("Cutting",
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 21,
-                                          fontWeight: FontWeight.bold)),
+                                Container(
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: AssetImage("assets/max2.png"),
+                                          fit: BoxFit.fitHeight)),
                                 ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.only(left: 10.0),
+                                      child: Text(state.view!.data![index].service!,
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 21,
+                                              fontWeight: FontWeight.bold)),
+                                    ),
+                                  ],
+                                )
                               ],
-                            )
-                          ],
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  );
-                })
+                      );
+                    });
+              } else if (state is ViewServicesError) {
+                return Center(
+                  child: Text(state.error),
+                );
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.pinkAccent,
+                  ),
+                );
+              }
+            })
           ],
         ),
       ),
